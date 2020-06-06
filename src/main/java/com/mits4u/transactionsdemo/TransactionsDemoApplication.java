64f09d69eebb33ef3753e241e83142518b8e5d17
Edvinas Veretinskas <ed@mits4u.co.uk>
2020-06-06 22:56:25 +0100

    Initial project  from start.spring.io

---
+++ src/main/java/com/mits4u/transactionsdemo/TransactionsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mits4u.transactionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mits4u/transactionsdemo/TransactionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mits4u.transactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
